78a2c5c2470b853d6405610c34e3ecb464496c21
Abhishek Shende <abhshekisnot@gmail.com>
2014-07-30 17:59:31 +0000

    Import of ABHIISNOT/Mojolicious-Plugin-REST-0.004 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-REST
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         ABHIISNOT/Mojolicious-Plugin-REST-0.004.tar.gz
    gitpan-cpan-author:       ABHIISNOT
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Controller/REST.pm
+++ lib/Mojolicious/Controller/REST.pm
@@ -1,52 +1,53 @@
-# ABSTRACT: Mojolicious Controller for RESTful operations
 package Mojolicious::Controller::REST;
 
+# ABSTRACT: Mojolicious Controller for RESTful operations
+our $VERSION = '0.004'; # VERSION
 use Mojo::Base 'Mojolicious::Controller';
 
 sub data {
-	my $self = shift;
-	my %data = @_;
+    my $self = shift;
+    my %data = @_;
 
-	my $json = $self->stash('json');
+    my $json = $self->stash('json');
 
-	if ( defined( $json->{data} ) ) {
-		@{ $json->{data} }{ keys %data } = values %data;
-	}
-	else {
-		$json->{data} = {%data};
-	}
+    if ( defined( $json->{data} ) ) {
+        @{ $json->{data} }{ keys %data } = values %data;
+    }
+    else {
+        $json->{data} = {%data};
+    }
 
-	$self->stash( json => $json );
-	return $self;
+    $self->stash( json => $json );
+    return $self;
 
 }
 
 sub message {
-	my $self = shift;
-	my ( $message, $severity ) = @_;
+    my $self = shift;
+    my ( $message, $severity ) = @_;
 
-	$severity //= 'info';
+    $severity //= 'info';
 
-	my $json = $self->stash('json');
+    my $json = $self->stash('json');
 
-	if ( defined( $json->{messages} ) ) {
-		push( $json->{messages}, { text => $message, severity => $severity } );
-	}
-	else {
-		$json->{messages} = [ { text => $message, severity => $severity } ];
-	}
+    if ( defined( $json->{messages} ) ) {
+        push( $json->{messages}, { text => $message, severity => $severity } );
+    }
+    else {
+        $json->{messages} = [ { text => $message, severity => $severity } ];
+    }
 
-	$self->stash( json => $json );
-	return $self;
+    $self->stash( json => $json );
+    return $self;
 }
 
 sub message_warn { $_[0]->message( $_[1], 'warn' ) }
 
 sub status {
-	my $self   = shift;
-	my $status = shift;
-	$self->stash( 'status' => $status );
-	return $self;
+    my $self   = shift;
+    my $status = shift;
+    $self->stash( 'status' => $status );
+    return $self;
 }
 
 1;
@@ -61,55 +62,55 @@ Mojolicious::Controller::REST - Mojolicious Controller for RESTful operations
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =head1 SYNOPSIS
 
-	# In Mojolicious Controller
-	use Mojo::Base 'Mojolicious::Controller::REST';
-	
-	$self->data( hello => 'world' )->message('Something went wrong');
-	
-	# renders json response as:
-	
-	{
-		"data":
-		{
-			"hello": "world"
-		},
-		"messages":
-		[
-			{
-				"severity": "info",
-				"text": "Something went wrong"
-			}
-		]
-	}
+    # In Mojolicious Controller
+    use Mojo::Base 'Mojolicious::Controller::REST';
+
+    $self->data( hello => 'world' )->message('Something went wrong');
+
+    # renders json response as:
+
+    {
+        "data":
+        {
+            "hello": "world"
+        },
+        "messages":
+        [
+            {
+                "severity": "info",
+                "text": "Something went wrong"
+            }
+        ]
+    }
 
 =head1 DESCRIPTION
 
-Mojolicious::Controller::REST helps with JSON rendering in RESTful applications. It follows  
+Mojolicious::Controller::REST helps with JSON rendering in RESTful applications. It follows
 and ensures the output of the method in controller adheres to the following output format as JSON:
 
-	{
-		"data":
-		{
-			"<key1>": "<value1>",
-			"<key2>": "<value2>",
-			...
-		},
-		"messages":
-		[
-			{
-				"severity": "<warn|info>",
-				"text": "<message1>"
-			},
-			{
-				"severity": "<warn|info>",
-				"text": "<message2>"
-			},
-		]
-	}
+    {
+        "data":
+        {
+            "<key1>": "<value1>",
+            "<key2>": "<value2>",
+            ...
+        },
+        "messages":
+        [
+            {
+                "severity": "<warn|info>",
+                "text": "<message1>"
+            },
+            {
+                "severity": "<warn|info>",
+                "text": "<message2>"
+            },
+        ]
+    }
 
 Mojolicious::Controller::REST extends Mojolicious::Controller and adds below methods
 
@@ -127,19 +128,19 @@ other method calls can be chained.
 
 A custom severity value can be used by calling message as:
 
-	$self->message('Something went wrong', 'fatal');
+    $self->message('Something went wrong', 'fatal');
 
-	# renders json response as:
-	
-	{
-		"messages":
-		[
-			{
-				"text": "Something went wrong",
-				"severity": "fatal"
-			}
-		]
-	}
+    # renders json response as:
+
+    {
+        "messages":
+        [
+            {
+                "text": "Something went wrong",
+                "severity": "fatal"
+            }
+        ]
+    }
 
 =head2 message_warn
 
--- lib/Mojolicious/Plugin/REST.pm
+++ lib/Mojolicious/Plugin/REST.pm
@@ -1,133 +1,141 @@
-# ABSTRACT: Mojolicious Plugin for RESTful operations
 package Mojolicious::Plugin::REST;
+
+# ABSTRACT: Mojolicious Plugin for RESTful operations
+our $VERSION = '0.004'; # VERSION
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::Exception;
 use Lingua::EN::Inflect 1.895 qw/PL/;
 
 my $http2crud = {
-	get    => 'read',
-	post   => 'create',
-	put    => 'update',
-	delete => 'delete',
-	list   => 'list',
+    get    => 'read',
+    post   => 'create',
+    put    => 'update',
+    delete => 'delete',
+    list   => 'list',
 };
 
 sub register {
-	my $self    = shift;
-	my $app     = shift;
-	my $options = { @_ ? ( ref $_[0] ? %{ $_[0] } : @_ ) : () };
+    my $self         = shift;
+    my $app          = shift;
+    my $options      = { @_ ? ( ref $_[0] ? %{ $_[0] } : @_ ) : () };
+    my $route_prefix = '';
 
-	foreach my $method ( keys %$http2crud ) {
-		$options->{http2crud}->{$method} = $http2crud->{$method} unless exists $options->{http2crud}->{$method};
-	}
+    foreach my $modifier (qw(prefix version)) {
+        if ( defined $options->{$modifier} && $options->{prefix} ne '' ) {
+            $route_prefix .= "/" . $options->{$modifier};
+        }
+    }
 
-	$app->hook(
-		before_render => sub {
-			my $c = shift;
+    foreach my $method ( keys %$http2crud ) {
+        $options->{http2crud}->{$method} = $http2crud->{$method} unless exists $options->{http2crud}->{$method};
+    }
 
-			my $json = $c->stash('json');
+    $app->hook(
+        before_render => sub {
+            my $c = shift;
+            my $path_substr = substr "" . $c->req->url->path, 0, length $route_prefix;
+            if ( $path_substr eq $route_prefix ) {
+                my $json = $c->stash('json');
 
-			unless ( defined $json->{data} ) {
-				$json->{data} = {};
-				$c->stash( 'json' => $json );
-			}
-			unless ( defined $json->{messages} ) {
-				$json->{messages} = [];
-				$c->stash( 'json' => $json );
-			}
-		}
-	);
+                unless ( defined $json->{data} ) {
+                    $json->{data} = {};
+                    $c->stash( 'json' => $json );
+                }
+                unless ( defined $json->{messages} ) {
+                    $json->{messages} = [];
+                    $c->stash( 'json' => $json );
+                }
+            }
+        }
+    );
 
-	$app->routes->add_shortcut(
-		rest_routes => sub {
-			my $routes = shift;
-			my $params = { @_ ? ( ref $_[0] ? %{ $_[0] } : @_ ) : () };
+    $app->routes->add_shortcut(
+        rest_routes => sub {
+            my $routes = shift;
+            my $params = { @_ ? ( ref $_[0] ? %{ $_[0] } : @_ ) : () };
 
-			Mojo::Exception->throw('Route name is required in rest_routes') unless defined $params->{name};
+            Mojo::Exception->throw('Route name is required in rest_routes') unless defined $params->{name};
 
-			my ( $destination, $route_prefix, $separator, $under_lower, $under_plural, $under_id, $resource ) = ( '', '', '_' );
-			##
-			my $name        = $params->{name};
-			my $name_lower  = lc $name;
-			my $name_plural = PL( $name_lower, 10 );
-			my $route_id    = ":" . $name_lower . "Id";
-			#
-			my $under = $params->{under};
-			if ( defined($under) ) {
-				$under_lower  = lc $under;
-				$under_plural = PL( $under_lower, 10 );
-				$under_id     = ":" . $under_lower . "Id";
-				$separator    = "_" . $under_lower . "_";
-			}
+            my ( $destination, $separator, $under_lower, $under_plural, $under_id, $resource ) = ( '', '_' );
+            ##
+            my $name        = $params->{name};
+            my $name_lower  = lc $name;
+            my $name_plural = PL( $name_lower, 10 );
+            my $route_id    = ":" . $name_lower . "Id";
+            #
+            my $under = $params->{under};
+            if ( defined($under) ) {
+                $under_lower  = lc $under;
+                $under_plural = PL( $under_lower, 10 );
+                $under_id     = ":" . $under_lower . "Id";
+                $separator    = "_" . $under_lower . "_";
+            }
 
-			#
-			my $readonly = $params->{readonly} // 0;
-			#
-			foreach my $modifier (qw(prefix version)) {
-				if ( defined $options->{$modifier} && $options->{prefix} ne '' ) {
-					$route_prefix .= "/" . $options->{$modifier};
-				}
-			}
-			##
-			my $controller = $params->{controller} // "$name#";
+            #
+            my $readonly = $params->{readonly} // 0;
+            ##
+            my $controller = $params->{controller} // "$name#";
 
-			# Install routes for resource collection #
-			if ( defined($under) ) {
-				$resource = $routes->route("$route_prefix/$under_plural/$under_id/$name_plural")->to($controller)->name("$under_lower$name_plural");
-			}
-			else {
-				$resource = $routes->route("$route_prefix/$name_plural")->to($controller)->name($name_plural);
-			}
+            # Install routes for resource collection #
+            if ( defined($under) ) {
+                $resource = $routes->route("$route_prefix/$under_plural/$under_id/$name_plural")->to($controller)
+                    ->name("$under_lower$name_plural");
+            }
+            else {
+                $resource = $routes->route("$route_prefix/$name_plural")->to($controller)->name($name_plural);
+            }
 
-			# GET resource collection
-			$destination = $options->{http2crud}->{list} . $separator . $name_plural;
-			$resource->get->to( '#' . $destination )->name($destination);
+            # GET resource collection
+            $destination = $options->{http2crud}->{list} . $separator . $name_plural;
+            $resource->get->to( '#' . $destination )->name($destination);
 
-			# POST to resource collection
-			if ( !$readonly ) {
-				$destination = $options->{http2crud}->{post} . $separator . $name_lower;
-				$resource->post->to( '#' . $destination )->name($destination);
-			}
+            # POST to resource collection
+            if ( !$readonly ) {
+                $destination = $options->{http2crud}->{post} . $separator . $name_lower;
+                $resource->post->to( '#' . $destination )->name($destination);
+            }
 
-			# Install routes for single resource #
-			my $ids = [];
-			if ( defined( $params->{types} ) ) {
-				$ids = $params->{types};
-			}
-			else {
-				push @$ids, ":" . $name_lower . "Id";
-			}
+            # Install routes for single resource #
+            my $ids = [];
+            if ( defined( $params->{types} ) ) {
+                $ids = $params->{types};
+            }
+            else {
+                push @$ids, ":" . $name_lower . "Id";
+            }
 
-			foreach my $route_id (@$ids) {
-				if ( defined( $params->{types} ) ) {
-					$controller = $params->{controller} // $name . '::' . ucfirst($route_id) . "#";
-				}
-				if ( defined($under) ) {
-					$resource = $routes->route( "$route_prefix/$under_plural/$under_id/$name_plural/$route_id", "$route_id" => qr/\d+/ )
-						->to( $controller, idname => "$route_id" )->name("$under_lower$name_lower");
-				}
-				else {
-					$resource = $routes->route( "$route_prefix/$name_plural/$route_id", "$route_id" => qr/\d+/ )->to( $controller, idname => "$route_id" )->name("$name_lower");
-				}
+            foreach my $route_id (@$ids) {
+                if ( defined( $params->{types} ) ) {
+                    $controller = $params->{controller} // $name . '::' . ucfirst($route_id) . "#";
+                }
+                if ( defined($under) ) {
+                    $resource = $routes->route( "$route_prefix/$under_plural/$under_id/$name_plural/$route_id",
+                        "$route_id" => qr/\d+/ )
+                        ->to( $controller, idname => "$route_id" )->name("$under_lower$name_lower");
+                }
+                else {
+                    $resource = $routes->route( "$route_prefix/$name_plural/$route_id", "$route_id" => qr/\d+/ )
+                        ->to( $controller, idname => "$route_id" )->name("$name_lower");
+                }
 
-				# GET a single resource
-				$destination = $options->{http2crud}->{get} . $separator . $name_lower;
-				$resource->get->to( '#' . $destination )->name($destination);
-				
-				if ( !$readonly ) {
+                # GET a single resource
+                $destination = $options->{http2crud}->{get} . $separator . $name_lower;
+                $resource->get->to( '#' . $destination )->name($destination);
 
-					# PUT requests - updates a resource
-					$destination = $options->{http2crud}->{put} . $separator . $name_lower;
-					$resource->put->to( '#' . $destination )->name($destination);
+                if ( !$readonly ) {
 
-					# DELETE requests - updates a resource
-					$destination = $options->{http2crud}->{delete} . $separator . $name_lower;
-					$resource->delete->to( '#' . $destination )->name($destination);
-				}
+                    # PUT requests - updates a resource
+                    $destination = $options->{http2crud}->{put} . $separator . $name_lower;
+                    $resource->put->to( '#' . $destination )->name($destination);
 
-			}
-		}
-	);
+                    # DELETE requests - updates a resource
+                    $destination = $options->{http2crud}->{delete} . $separator . $name_lower;
+                    $resource->delete->to( '#' . $destination )->name($destination);
+                }
+
+            }
+        }
+    );
 }
 
 1;
@@ -142,54 +150,54 @@ Mojolicious::Plugin::REST - Mojolicious Plugin for RESTful operations
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =head1 SYNOPSIS
 
-	# In Mojolicious application
-	$self->plugin( 'REST', { prefix => 'api', version => 'v1', } );
-	
-	$self->routes->rest_routes( name => 'Account' );
-	
-	# Installs following routes:
+    # In Mojolicious application
+    $self->plugin( 'REST', { prefix => 'api', version => 'v1', } );
+
+    $self->routes->rest_routes( name => 'Account' );
+
+    # Installs following routes:
     # +-------------+-----------------------------+-------------------------+
-	# | HTTP Method |             URL             |          Route          |
-	# +-------------+-----------------------------+-------------------------+
-	# | GET         | /api/v1/accounts            | Account::list_accounts  |
-	# | POST        | /api/v1/accounts            | Account::create_account |
-	# | GET         | /api/v1/accounts/:accountId | Account::read_account   |
-	# | PUT         | /api/v1/accounts/:accountId | Account::update_account |
-	# | DELETE      | /api/v1/accounts/:accountId | Account::delete_account |
-	# +-------------+-----------------------------+-------------------------+
-	
-	$routes->rest_routes( name => 'Feature', under => 'Account' );
-	
-	# Installs following routes:
-	# +-------------+-------------------------------------------------+---------------------------------+
-	# | HTTP Method |                       URL                       |              Route              |
-	# +-------------+-------------------------------------------------+---------------------------------+
-	# | GET         | /api/v1/accounts/:accountId/features            | Feature::list_account_features  |
-	# | POST        | /api/v1/accounts/:accountId/features            | Feature::create_account_feature |
-	# | GET         | /api/v1/accounts/:accountId/features/:featureId | Feature::read_account_feature   |
-	# | PUT         | /api/v1/accounts/:accountId/features/:featureId | Feature::update_account_feature |
-	# | DELETE      | /api/v1/accounts/:accountId/features/:featureId | Feature::delete_account_feature |
-	# +-------------+-------------------------------------------------+---------------------------------+
-	
-	$routes->rest_routes( name => 'Product', under => 'Account', types => [qw(FTP SSH)] );
-	
-	# Installs following routes:	
-	# +-------------+------------------------------------------+--------------------------------------+
-	# | HTTP Method |                   URL                    |                Route                 |
-	# +-------------+------------------------------------------+--------------------------------------+
-	# | GET         | /api/v1/accounts/:accountId/products     | Product::list_account_products       |
-	# | POST        | /api/v1/accounts/:accountId/products     | Product::create_account_product      |
-	# | GET         | /api/v1/accounts/:accountId/products/FTP | Product::FTP::read_account_product   |
-	# | PUT         | /api/v1/accounts/:accountId/products/FTP | Product::FTP::update_account_product |
-	# | DELETE      | /api/v1/accounts/:accountId/products/FTP | Product::FTP::delete_account_product |
-	# | GET         | /api/v1/accounts/:accountId/products/SSH | Product::SSH::read_account_product   |
-	# | PUT         | /api/v1/accounts/:accountId/products/SSH | Product::SSH::update_account_product |
-	# | DELETE      | /api/v1/accounts/:accountId/products/SSH | Product::SSH::delete_account_product |
-	# +-------------+------------------------------------------+--------------------------------------+
+    # | HTTP Method |             URL             |          Route          |
+    # +-------------+-----------------------------+-------------------------+
+    # | GET         | /api/v1/accounts            | Account::list_accounts  |
+    # | POST        | /api/v1/accounts            | Account::create_account |
+    # | GET         | /api/v1/accounts/:accountId | Account::read_account   |
+    # | PUT         | /api/v1/accounts/:accountId | Account::update_account |
+    # | DELETE      | /api/v1/accounts/:accountId | Account::delete_account |
+    # +-------------+-----------------------------+-------------------------+
+
+    $routes->rest_routes( name => 'Feature', under => 'Account' );
+
+    # Installs following routes:
+    # +-------------+-------------------------------------------------+---------------------------------+
+    # | HTTP Method |                       URL                       |              Route              |
+    # +-------------+-------------------------------------------------+---------------------------------+
+    # | GET         | /api/v1/accounts/:accountId/features            | Feature::list_account_features  |
+    # | POST        | /api/v1/accounts/:accountId/features            | Feature::create_account_feature |
+    # | GET         | /api/v1/accounts/:accountId/features/:featureId | Feature::read_account_feature   |
+    # | PUT         | /api/v1/accounts/:accountId/features/:featureId | Feature::update_account_feature |
+    # | DELETE      | /api/v1/accounts/:accountId/features/:featureId | Feature::delete_account_feature |
+    # +-------------+-------------------------------------------------+---------------------------------+
+
+    $routes->rest_routes( name => 'Product', under => 'Account', types => [qw(FTP SSH)] );
+
+    # Installs following routes:
+    # +-------------+------------------------------------------+--------------------------------------+
+    # | HTTP Method |                   URL                    |                Route                 |
+    # +-------------+------------------------------------------+--------------------------------------+
+    # | GET         | /api/v1/accounts/:accountId/products     | Product::list_account_products       |
+    # | POST        | /api/v1/accounts/:accountId/products     | Product::create_account_product      |
+    # | GET         | /api/v1/accounts/:accountId/products/FTP | Product::FTP::read_account_product   |
+    # | PUT         | /api/v1/accounts/:accountId/products/FTP | Product::FTP::update_account_product |
+    # | DELETE      | /api/v1/accounts/:accountId/products/FTP | Product::FTP::delete_account_product |
+    # | GET         | /api/v1/accounts/:accountId/products/SSH | Product::SSH::read_account_product   |
+    # | PUT         | /api/v1/accounts/:accountId/products/SSH | Product::SSH::update_account_product |
+    # | DELETE      | /api/v1/accounts/:accountId/products/SSH | Product::SSH::delete_account_product |
+    # +-------------+------------------------------------------+--------------------------------------+
 
 =head1 DESCRIPTION
 
@@ -197,9 +205,10 @@ L<Mojolicious::Plugin::REST> adds various helpers for L<REST|http://en.wikipedia
 L<CRUD|http://en.wikipedia.org/wiki/Create,_read,_update_and_delete> operations via
 L<HTTP|http://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol> to the app.
 
-As much as possible, it tries to follow L<RESTful API Design|https://blog.apigee.com/detail/restful_api_design> principles from Apigee.
+As much as possible, it tries to follow L<RESTful API Design|https://blog.apigee.com/detail/restful_api_design>
+principles from Apigee.
 
-Used in conjuction with L<Mojolicious::Controller::REST>, this module makes building RESTful application a breeze. 
+Used in conjuction with L<Mojolicious::Controller::REST>, this module makes building RESTful application a breeze.
 
 This module is inspired from L<Mojolicious::Plugin::RESTRoutes>.
 
@@ -211,7 +220,8 @@ A routes shourtcut to easily add RESTful routes for a resource.
 
 =head1 MOJOLICIOUS HOOKS
 
-This module installs an before_render application hook, which gurantees JSON output. Refer L<Mojolicious::Controller::REST> documentation for output format.
+This module installs an before_render application hook, which gurantees JSON output. Refer L<Mojolicious::Controller::REST>
+documentation for output format.
 
 =head1 OPTIONS
 
@@ -249,7 +259,7 @@ This option can be used to specify types of resources available in application.
 
 =item prefix
 
-If present, this option will be added before every route created. 
+If present, this option will be added before every route created.
 
 =item version
 
@@ -259,17 +269,31 @@ If present, api version given will be added before every route created (but afte
 
 If present, given HTTP to CRUD mapping will be used to determine method names. Default mapping:
 
-	get     ->  read
-	post    ->  create
-	put     ->  update
-	delete  ->  delete
-	list    ->  list
+    get     ->  read
+    post    ->  create
+    put     ->  update
+    delete  ->  delete
+    list    ->  list
 
 =back
 
 =head1 AUTHOR
 
 Abhishek Shende <abhishekisnot@gmail.com>
+
+=head1 CONTRIBUTORS
+
+=over 4
+
+=item *
+
+Abhishek Shende <a.shende@contractor.verio.net>
+
+=item *
+
+Vincent HETRU <vincent.hetru@13pass.com>
+
+=back
 
 =head1 COPYRIGHT AND LICENSE
 
